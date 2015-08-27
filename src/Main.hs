38d3772ae019167f55046d1323adc36db17aac3e
jwsloan <jwsloan@gmail.com>
2015-08-26 23:16:49 -0500

    Cleanup main

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,12 @@
 -----------------------------------------------------------------------------
 --
 -- Module      :  Main
--- Copyright   :  
+-- Copyright   :
 -- License     :  AllRightsReserved
 --
--- Maintainer  :  
--- Stability   :  
--- Portability :  
+-- Maintainer  :
+-- Stability   :
+-- Portability :
 --
 -- |
 --
@@ -17,33 +17,12 @@ module Main (
     main
 ) where
 
-import Control.Monad (unless)
-import Data.List (stripPrefix)
 import System.Exit (exitFailure)
-import Test.QuickCheck.All (quickCheckAll)
+import MealPlanner
 
--- Simple function to create a hello message.
-hello s = "Hello " ++ s
 
--- Tell QuickCheck that if you strip "Hello " from the start of
--- hello s you will be left with s (for any s).
-prop_hello s = stripPrefix "Hello " (hello s) == Just s
+main = do
+    let aldi = GroceryList [("ingredient", "store", 1, Cups)]
+    putStrLn ("Hello World")
 
--- Hello World
-exeMain = do
-    putStrLn (hello "World")
-
--- Entry point for unit tests.
-testMain = do
-    allPass <- $quickCheckAll -- Run QuickCheck on all prop_ functions
-    unless allPass exitFailure
-
--- This is a clunky, but portable, way to use the same Main module file
--- for both an application and for unit tests.
--- MAIN_FUNCTION is preprocessor macro set to exeMain or testMain.
--- That way we can use the same file for both an application and for tests.
-#ifndef MAIN_FUNCTION
-#define MAIN_FUNCTION exeMain
-#endif
-main = MAIN_FUNCTION
 
--- src/MealPlanner.hs
+++ src/MealPlanner.hs
@@ -8,8 +8,16 @@
 -- Stability   :
 -- Portability :
 --
--- |
+-- First Step - clean up the raw ingredient list
+------ Commonize all of the names of ingredients
+-- Second Step - Read in list of ingredients, and combine the counts. Maybe also add price
 --
+-- Some items can substitute for others
+-- Keep track of price while at the store
+-- Each ingredient should have a designated store, backup store, and section
+-- An ingredient could have a "Costco threshold"
+-- How to model one recipe spanning multiple meals?
+-- How to model 1 bag of 3 peppers at Alid vs 3 single peppers at Kroger vs 1 bag of 6 peppers at Costco
 -----------------------------------------------------------------------------
 
 module MealPlanner (
@@ -17,11 +25,21 @@ module MealPlanner (
 ) where
 
 type Name = String
-type Amount = Integer
-type Isle = String
+type Amount = Float
+type Section = String
+type Cost = Float
+type Per = Integer
+type BackupStore = Maybe Store
+type Ingredient = (Name, Section)
 
-data UOM = Cups | Lbs | Ozs | Tsp | Tbsp deriving (Show, Eq)
-data Ingredient = Ingredient Name UOM deriving (Show, Eq)
-data Ingredients = Ingredients [(Ingredient, Amount)] deriving (Show)
-data Recipe = Recipe Name Ingredients deriving (Show)
+
+data GroceryList = GroceryList [(Ingredient, Store, Amount, UOM)]
+data Store = Name [(Ingredient, Cost, UOM, Per)] BackupStore deriving (Show, Eq)
+data UOM = Cups | Lbs | Ozs | Tsp | Tbsp | Each | Bag deriving (Show, Eq)
+data Recipe = Recipe Name [(Ingredient, Amount, UOM)] deriving (Show)
+data Meal = Meal Name [Recipe] deriving (Show) --Add days
+
+
+
+
 
